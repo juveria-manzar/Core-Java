6ed3c4197522cd78c716a70652441d8fad427943
JUVERIA MANZAR <juveriamanzar29@gmail.com>
2021-02-19 03:44:09 +0530

    Updated

---
+++ ArrayTest.java
@@ -0,0 +1,16 @@
+public class ArrayTest {
+    public static void main(String[] args) {
+        boolean[] bArray=new boolean[5];
+        System.out.println(bArray[0]);
+        char[] cArray=new char[5];
+        System.out.println(cArray[0]);
+        float[] fArray=new float[5];
+        System.out.println(fArray[0]);
+        double[] dArray=new double[5];
+        System.out.println(dArray[0]);
+
+        int[] arr = new int[5];
+        arr = new int[6];
+        System.out.println(arr.length);
+    }
+}
